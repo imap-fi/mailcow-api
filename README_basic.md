1dea71423f17e1e87f74fdefb9427e9cd9411fdb
Paul Colin Hennig <8d_fvfmaa-o2wf_79aqig6g2ki6-09ffkeqmyo3d@firstdorsal.eu>
2020-09-04 17:28:12 +0200

    added functions

---
+++ README_basic.md
@@ -0,0 +1,81 @@
+# Install
+```
+npm i mailcow-api
+```
+
+# Basic Example
+```js
+(async () => {
+    //get global variables
+    require('dotenv').config();
+    
+    //import the api client module
+    const {
+        MailcowApiClient
+    } = require("mailcow-api")
+
+    //create new mailcow api client with endpoint/baseurl and the api key
+    const mcc = new MailcowApiClient(process.env.MAILCOW_API_BASEURL, process.env.MAILCOW_API_KEY);
+
+    //get all domain on mailcow server
+    console.log(await mcc.getDomain());
+})();
+```
+
+## What is dotenv?
+The line "require('dotenv').config();" gets the contents of a file called ".env" in which you should store your global and secret variables.
+
+### 1. Install the module "dotenv" with
+```
+npm i dotenv
+```
+### 2. Create a file named ".env" in your applications root directory
+
+*.env*
+```c
+MAILCOW_API_KEY='YOUR MAILCOW API KEY'
+MAILCOW_API_BASEURL='https://mail.example.com' //no trailing slashes
+```
+### 3. Use your secret variables 
+```
+process.env.MAILCOW_API_BASEURL
+process.env.MAILCOW_API_KEY
+```
+
+# Where to get the API key?
+### 1. Open your mailcow UI and login as admin
+
+
+### 1.1 Are you using two factor authentication for your admin account?
+If not: **Do it now!** It's easy! For Android you can use the andOTP app.
+andOTP can be used for 2FA with many services and is way better then the Google Authenticator app.
+
+### 2. Scroll to and expand the API section
+
+### 3. Insert the IP you want to accesss the API from or disable the API check
+
+### 4. Tick the checkbox "Activate API" and save the settings
+
+### 5. Copy your API key from the field above
+
+
+# Documentation
+**[Documentation for THIS wrapper](https://firstdorsal.eu/doc/mailcow-api/)**
+
+# Need help or missing a feature?
+Feel free to contact me via [xl9jthv_7bvgakv9o9wg0jabn2ylm91xxrzzgt0e@firstdorsal.eu](mailto:xl9jthv_7bvgakv9o9wg0jabn2ylm91xxrzzgt0e@firstdorsal.eu) in english or german
+
+## Mailcow API Documentation
+[Apiary](https://mailcow.docs.apiary.io/)
+
+[Swagger](https://demo.mailcow.email/api/)
+
+## Links
+[NPM](https://www.npmjs.com/package/mailcow-api)
+
+[Documentation](https://firstdorsal.eu/doc/mailcow-api/)
+
+[Code on Gitlab](https://git.firstdorsal.eu/firstdorsal/mailcow-api)
+
+
+
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 const f = require("node-fetch")
-
+const rs = require("randomstring");
 
 /**
  * For all options check out {@link https://demo.mailcow.email/api/}
@@ -33,20 +33,81 @@ const f = require("node-fetch")
  */
 
 
+/**
+* Object representing a DKIM Key
+* @typedef DKIM
+* @type {Object}
+* @prop {String} domain The domain which a key should be generated for
+* @prop {String} [dkim_selector='dkim'] The dkim selector
+* @prop {2048|1024} [key_size=2048] The size of the key
+* @example
+ {
+  "domain": "example.com",
+  "dkim_selector": "dkim",
+  "key_size": "2048"
+ }
+*/
+
+/**
+* Object representing a domain admin
+* @typedef DomainAdmin
+* @type {Object}
+* @prop {Array|String} domains The domains/domain this admin should be able to access
+* @prop {String} [username=RANDOM]
+* @prop {String} [password=RANDOM] 
+* @prop {0|1} [active=1]
+* @example
+ {
+  "active": 1,
+  "domains": "example.com",
+  "password": "supersecurepw",
+  "username": "testadmin"
+}
+*/
+
+/**
+ * Object representing a mailbox
+ * @typedef Mailbox
+ * @type {Object}
+ * @prop {String} domain domain for wich the mailbox shall be created
+ * @prop {String} [local_part="mail"] the local part of the mail address before the @
+ * @prop {String} [name="John Doe"] full name of the user
+ * @prop {String} [password=RANDOM] password for the user. if omitted one will be generated
+ * @prop {Number} [quota=3072] maximum size of the mailbox
+ * @prop {0|1} [active=1] whether the mailbox is active or not
+ * @example
+ {
+  "domain": "example.com",
+  "local_part": "john.doe",
+  "name": "John Doe",
+  "password": "paulIstToll",
+  "quota": "3072",
+  "active": 1
+}
+ 
+ * */
+
+
+
+
 /** @module mailcow-api */
 /** 
  * @class Class representing the Mailcow API client 
  *@example 
 (async () => {
+    //get global variables
     require('dotenv').config();
-
-        const {
+    
+    //import the api client module
+    const {
         MailcowApiClient
-    } = require("./index.js")
+    } = require("mailcow-api")
 
-    const mc = new MailcowApiClient(process.env.MAILCOW_API_BASEURL, process.env.MAILCOW_API_KEY);
+    //create new mailcow api client with endpoint/baseurl and the api key
+    const mcc = new MailcowApiClient(process.env.MAILCOW_API_BASEURL, process.env.MAILCOW_API_KEY);
 
-    console.log(await mc.getDomain());
+    //get all domain on mailcow server
+    console.log(await mcc.getDomain());
 })();
 */
 
@@ -57,7 +118,7 @@ module.exports.MailcowApiClient = class {
      * @param {string} baseurl The base url where the api can be found
      * @param {string} apikey The api key for the mailcow api endpoint
      * @example
-     * const mc = new MailcowApiClient(process.env.MAILCOW_API_BASEURL, process.env.MAILCOW_API_KEY);
+     * const mcc = new MailcowApiClient(process.env.MAILCOW_API_BASEURL, process.env.MAILCOW_API_KEY);
      */
     constructor(baseurl, apikey) {
         this.baseurl = baseurl;
@@ -68,7 +129,7 @@ module.exports.MailcowApiClient = class {
      * @param {String} [domain='all'] The domain you want to get
      * @returns {Array} Array of domains
      * @example
-     * await mc.getDomain()
+     * await mcc.getDomain()
      * */
     async getDomain(domain) {
         if (!domain || !domain.length) domain = "all"
@@ -80,7 +141,8 @@ module.exports.MailcowApiClient = class {
         }).then(async (res) => {
             const j = await res.json().catch();
             if (!j.length) return [j]
-            return j;
+            console.error(j);
+            return false;
         })
     }
     /**
@@ -88,14 +150,14 @@ module.exports.MailcowApiClient = class {
      * @param {Domain} domain The domain you want to set add
      * @returns {Boolean} True on success
      * @example
-        await mc.addDomain({
+        await mcc.addDomain({
             domain: "example.com",
         }))
      * */
     async addDomain(domain) {
         if (!domain) throw new Error('Missing Domain Object');
         if (!domain.domain) throw new Error('Domain object must contain a value for domain. Example: {domain:"example.com"}');
-        if (!domain.domain.match(/[A-Za-z0-9]+\.[A-Za-z0-9]+$/)) throw new Error('domain is invalid');
+        if (!domain.domain.match(/[A-Za-z0-9]+\.[A-Za-z0-9]+$/)) throw new Error('domain name is invalid');
         domain.active = typeof (domain.active) == 'undefined' ? 1 : domain.active;
         domain.aliases = typeof (domain.aliases) == 'undefined' ? 400 : domain.aliases;
         domain.defquota = typeof (domain.defquota) == 'undefined' ? 3072 : domain.defquota;
@@ -113,6 +175,7 @@ module.exports.MailcowApiClient = class {
         }).then(async (res) => {
             const j = await res.json().catch();
             if (j[0].type === 'success') return true;
+            console.error(j);
             return false;
         });
     }
@@ -122,20 +185,17 @@ module.exports.MailcowApiClient = class {
      * @param {Object} attributes Attributes to change for all domains provided domains
      * @returns {Boolean} True on success
      * @example
-        await mc.editDomain(["example.com"], {
+        await mcc.editDomain(["example.com"], {
             aliases: 399
         });
         //This will set the aliases of example.com to 399
      */
     async editDomain(domains, attributes) {
-        if (typeof domains === 'string') domains = [domains]
-
-
+        if (typeof domains === 'string') domains = [domains];
         const body = {
             items: domains,
             attr: attributes
         }
-
         return f(`${this.baseurl}/api/v1/edit/domain`, {
             method: 'POST',
             headers: {
@@ -146,9 +206,180 @@ module.exports.MailcowApiClient = class {
         }).then(async (res) => {
             const j = await res.json().catch();
             if (j[0].type === 'success') return true;
+            console.error(j);
             return false;
         });
     }
+
+    /**
+     * Generates a DKIM domain key for a domain
+     * @param {DKIM} dkim A DKIM object 
+     * @returns {Boolean} True on success
+     * @example
+        await mcc.addDKIM({
+            domain: "example.com",
+        })
+        //This will generate a DKIM key for example.com on the mailcow server
+     */
+    async addDKIM(dkim) {
+        dkim.domains = dkim.domain;
+        delete dkim.domain;
+        if (!dkim) throw new Error('DKIM Key must be provided as DKIM Object');
+        if (!dkim.domains) throw new Error('DKIM object must contain a domain name. Example: {domains:"example.com"}');
+        if (!dkim.domains.match(/[A-Za-z0-9]+\.[A-Za-z0-9]+$/)) throw new Error('domain name is invalid');
+        dkim.dkim_selector = typeof (dkim.dkim_selector) == 'undefined' ? 'dkim' : dkim.dkim_selector;
+        dkim.key_size = typeof (dkim.key_size) == 'undefined' ? 2048 : dkim.key_size;
+
+        return f(`${this.baseurl}/api/v1/add/dkim`, {
+            method: 'POST',
+            headers: {
+                'X-Api-Key': this.apikey,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dkim)
+        }).then(async (res) => {
+            const j = await res.json().catch();
+            if (j && j[0] && j[0].type === 'success') return true;
+            console.error(j);
+            return false;
+        });
+
+    }
+    /**
+     * Gets the DKIM key for a domain on the mailcow server
+     * @param {String} domain the domain name you want to get the key for
+     * @returns {Object} The DKIM public key and other parameters
+     * @example
+        await mcc.getDKIM('example.com')
+        //This will get the DKIM key for the domain example.com from the mailcow server
+     */
+    async getDKIM(domain) {
+        return f(`${this.baseurl}/api/v1/get/dkim/${domain}`, {
+            method: 'GET',
+            headers: {
+                'X-Api-Key': this.apikey
+            }
+        }).then(async (res) => {
+            const j = await res.json().catch();
+            if (j) return j
+            console.error(j);
+            return false;
+        });
+    }
+    /**
+     * Adds a domain admin to the mailcow server
+     * @param {DomainAdmin} domainAdmin a domain admin object that has to contain at least the domains the admin should be able to control
+     * @returns {Object} containing password username and domains on successfull creation
+     * @example
+        await mcc.addDomainAdmin({
+            domains: ['example.com', 'example.org']
+        })
+        //This will add an admin for the domains example.com and example.org and return their credentials
+     */
+    async addDomainAdmin(domainAdmin) {
+        if (!domainAdmin) throw new Error('Domain admin must be provided as DomainAdmin Object');
+        if (!domainAdmin.domains) throw new Error('Domain admin object must contain a domain name. Example: {domains:"example.com"}');
+        if (typeof domainAdmin.domains === 'string') domainAdmin.domains = [domainAdmin.domains];
+
+        if (!domainAdmin.domains[0].match(/[A-Za-z0-9]+\.[A-Za-z0-9]+$/)) throw new Error('domain name is invalid');
+
+        domainAdmin.active = typeof (domainAdmin.active) == 'undefined' ? 1 : domainAdmin.active;
+        domainAdmin.password = typeof (domainAdmin.password) == 'undefined' ? rs.generate(100) : domainAdmin.password;
+        domainAdmin.username = typeof (domainAdmin.username) == 'undefined' ? "domain_admin_" + rs.generate(20) : domainAdmin.username;
+        domainAdmin.password2 = domainAdmin.password;
+
+        return f(`${this.baseurl}/api/v1/add/domain-admin`, {
+            method: 'POST',
+            headers: {
+                'X-Api-Key': this.apikey,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(domainAdmin)
+        }).then(async (res) => {
+            const j = await res.json().catch();
+            if (j && j[0] && j[0].type === 'success') return {
+                username: domainAdmin.username,
+                password: domainAdmin.password,
+                domains: domainAdmin.domains
+            };
+            console.error(j);
+            return false;
+        });
+
+    }
+    /**
+     * Adds a mailbox for a domain to the mailcow server
+     * @param {Mailbox} mailbox a Mailbox object that has to contain at least the domain for which the mailbox shall be created
+     * @returns {Object} the created mailbox
+     * @example
+        await mcc.addMailbox({
+            domain: 'example.com',
+            name: 'Example'
+        })
+        //This will add a mailbox for the domain example.com and return it 
+     */
+    async addMailbox(mailbox) {
+        if (!mailbox) throw new Error('Mailbox must be provided as Mailbox Object');
+        if (!mailbox.domain) throw new Error('Mailbox object must at least contain a domain name. Example: {domain:"example.com"}');
+        if (!mailbox.domain.match(/[A-Za-z0-9]+\.[A-Za-z0-9]+$/)) throw new Error('domain name is invalid');
+
+        mailbox.active = typeof (mailbox.active) == 'undefined' ? 1 : mailbox.active;
+        mailbox.password = typeof (mailbox.password) == 'undefined' ? rs.generate(100) : mailbox.password;
+        mailbox.password2 = mailbox.password;
+        mailbox.quota = typeof (mailbox.quota) == 'undefined' ? 3072 : mailbox.quota;
+        mailbox.name = typeof (mailbox.name) == 'undefined' ? 'John Doe' : mailbox.name;
+        mailbox.local_part = typeof (mailbox.local_part) == 'undefined' ? 'mail' : mailbox.local_part;
+
+
+        return f(`${this.baseurl}/api/v1/add/mailbox`, {
+            method: 'POST',
+            headers: {
+                'X-Api-Key': this.apikey,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(mailbox)
+        }).then(async (res) => {
+            const j = await res.json().catch();
+            if (j && j[0] && j[0].type === 'success') return {
+                username: mailbox.local_part + '@' + mailbox.domain,
+                password: mailbox.password,
+                domain: mailbox.domain,
+                name: mailbox.name,
+                local_part: mailbox.local_part,
+                quota: mailbox.quota
+            };
+            console.error(j);
+            return false;
+        });
+    }
+    /**
+     * Deletes a mailbox 
+     * @param {String|Array} mailboxes complete name of the mailbox/mailboxes
+     * @returns {Boolean} true on success
+     * @example
+        await mcc.deleteMailbox("mail@example.com")
+        //This will delete the mailbox mail@example.com
+     */
+    async deleteMailbox(mailboxes) {
+        if (typeof mailboxes === 'string') mailboxes = [mailboxes];
+        if (!mailboxes[0].match(/[A-Za-z0-9]+\.[A-Za-z0-9]+$/)) throw new Error('domain name is invalid');
+
+        return f(`${this.baseurl}/api/v1/delete/mailbox`, {
+            method: 'POST',
+            headers: {
+                'X-Api-Key': this.apikey,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(mailboxes)
+        }).then(async (res) => {
+            const j = await res.json().catch();
+            if (j && j[0] && j[0].type === 'success') return true;
+            console.error(j);
+            return false;
+        });
+
+    }
+
 
 
 
